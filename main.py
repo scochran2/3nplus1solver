294074b2ae6c44c8dac017db5f7979de52049861
Seth Cochran <scochran2@protonmail.com>
2021-08-30 21:14:34 -0400

    Added while loop to repeat

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import math
-count = 0
-num = input("Enter any real number: ")
-num = float(num)
-while num != 1 and num != -1:
-    count += 1
-    if (num % 2) == 0:
-        num = (num / 2)
-        print("{:,.0f}".format(num))
-    else:
-        num = (num * 3) + 1
-        print("{:,.0f}".format(num))
-print("It took " + str(count) + " steps")
+while True:
+    import math
+    count = 0
+    num = input("Enter any real number: ")
+    num = float(num)
+    while num != 1 and num != -1:
+        count += 1
+        if (num % 2) == 0:
+            num = (num / 2)
+            print("{:,.0f}".format(num))
+        else:
+            num = (num * 3) + 1
+            print("{:,.0f}".format(num))
+    print("It took " + str(count) + " steps")
