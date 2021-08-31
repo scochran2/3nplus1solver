0a7acfa3630e059ea49b7e5f212c761ccabc1fa8
Seth Cochran <scochran2@protonmail.com>
2021-08-30 21:00:17 -0400

    Numerous changes to work with negative numbers

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-while True:
-    count = 0
-    num = input("Enter any real number: ")
-    num = int(num)
-    while num != 1:
-        count += 1
-        if (num % 2) == 0:
-            num = (num / 2)
-            print("{:,.0f}".format(num))
-        else:
-            num = (num * 3) + 1
-            print("{:,.0f}".format(num))
-    print("It took " + str(count) + " steps")
+count = 0
+num = input("Enter any real number: ")
+num = float(num)
+while num != 1 and num != -1:
+    count += 1
+    if (num % 2) == 0:
+        num = (num / 2)
+        print("{:,.0f}".format(num))
+    else:
+        num = (num * 3) + 1
+        print("{:,.0f}".format(num))
+print("It took " + str(count) + " steps")
